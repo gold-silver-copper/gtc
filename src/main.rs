5e0e640037322b7ca4332db54b4aa203b60bde1e
zombkit <stephen.korzen@gmail.com>
2025-02-10 17:24:22 -0800

    h

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
+use csv::Reader;
 use geojson::{Feature, FeatureCollection, GeoJson, Value};
 use std::error::Error;
 use std::fs::{create_dir, File};
 use std::io::Write;
 
+#[derive(Debug)]
+struct StopLocation {
+    id: String,
+    lat: f64,
+    lng: f64,
+}
+#[derive(Debug)]
+struct BlockLocation {
+    id: String,
+    population: i64,
+    lat: f64,
+    lng: f64,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // create_centroids_csv();
+    read_transit_stops();
     analysis();
     Ok(())
 }
 
 fn analysis() -> Result<(), Box<dyn Error>> {
     Ok(())
+}
+
+fn read_transit_stops() -> Result<Vec<StopLocation>, Box<dyn Error>> {
+    let mut reader = Reader::from_path("transit_stops_datasd.csv")?;
+    let mut locations = Vec::new();
+
+    for result in reader.records() {
+        let record = result?;
+        let id = record.get(1).unwrap_or("").to_string();
+        let lat: f64 = record.get(5).unwrap_or("0").parse()?;
+        let lng: f64 = record.get(6).unwrap_or("0").parse()?;
+        let geoloc = StopLocation { id, lat, lng };
+        println!("{:#?}", geoloc);
+        locations.push(geoloc);
+    }
+
+    Ok(locations)
 }
 
 fn create_centroids_csv() -> Result<(), Box<dyn Error>> {
